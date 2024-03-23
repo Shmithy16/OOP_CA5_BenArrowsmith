dd10663ebd8bdebfb6a5156ff66bbd1b5461510c
Shmithy16 <124367787+Shmithy16@users.noreply.github.com>
2024-03-23 14:50:06 +0000

    forgot to comment name for feature

--- src/main/java/DTOs/gameTitleComparator.java
+++ src/main/java/DTOs/gameTitleComparator.java
@@ -1,6 +1,8 @@
 package DTOs;
 import java.util.Comparator;
 
+
+//Ben Arrowsmith
 public class gameTitleComparator implements Comparator<Games>{
     public int compare(Games game1, Games game2)
     {
